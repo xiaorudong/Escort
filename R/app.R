c083934629b7c26564bf3ac95783237b6fcb7067
bahetyshyam <bahetyshyam@gmail.com>
2024-05-10 11:43:55 +0530

    Conditionally changing host based on development type

--- R/app.R
+++ R/app.R
@@ -8,7 +8,7 @@
 #' @import mclust
 #' @importFrom stats predict
 #' @importFrom scales alpha
-#' @importFrom dplyr first setequal intersect union 
+#' @importFrom dplyr first setequal intersect union
 #' @import magrittr
 #' @importFrom shinycssloaders withSpinner
 #' @import DT
@@ -20,17 +20,20 @@
 #'
 
 shinyEscort <- function(port = 19901, is_local_development = TRUE) {
-  #default port is set to 19901
-  #if is_local_development is set to false, then a browser window will
-  #not open up. We should use FALSE in production server
-  options(
-    shiny.maxRequestSize = 10000*1024^5,
+  # default port is set to 19901
+  # if is_local_development is set to false, then a browser window will
+  # not open up. We should use FALSE in production server
+  options_args <- list(
+    shiny.maxRequestSize = 10000 * 1024^5,
     shiny.launch.browser = is_local_development,
-    shiny.port = 19901,
-    shiny.host = "0.0.0.0",
+    shiny.port = port,
     test.mode = getOption("shiny.testmode", FALSE)
-    )
+  )
+  if (is_local_development == FALSE) {
+    options_args$shiny.host <- "0.0.0.0"
+  }
+  print(options_args)
+  do.call(options, options_args)
   library(Escort)
   shinyApp(ui, server)
 }
-
